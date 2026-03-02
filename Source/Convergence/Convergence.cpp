11fa319fab9bcc734c6c69f06744bc7e735cd7f2
Annastasia <a.w.school@outlook.com>
2026-03-02 10:45:54 +0100

    Added UProject

---
+++ Source/Convergence/Convergence.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Convergence.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Convergence, "Convergence" );
---
+++ Source/READ_ME_when_Renaming.txt
@@ -0,0 +1,6 @@
+Capitalize the project name in the following files:
+
+
+Source/<Project_Name>.cpp   - only as the 2nd argument, not the third!
+Source/Dummy.h
+Source/<Project_Name>.Build.cs
